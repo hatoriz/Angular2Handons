e806d186763c60067ffab8ec1c65b99472e6279c
Hatoriz Kwansiripat <ninja.hatoriz@gmail.com>
2017-04-01 11:39:43 +0700

    [TYPESCRIPT] - init tsconfig.json to separate js file from ts file

--- Sample01/js/taxCalc.js
+++ Sample01/js/taxCalc.js
@@ -1,28 +1,26 @@
-function calcTax(income: number, state: string = 'NY', dependents?: number):
-    number {
-    var deduction: number;
-
+function calcTax(income, state, dependents) {
+    if (state === void 0) { state = 'NY'; }
+    var deduction;
     if (dependents) {
         deduction = dependents * 500;
-    } else {
+    }
+    else {
         deduction = 0;
     }
-
     if (state == 'NY') {
         return income * 0.06 - deduction;
-    } else if (state == 'NJ') {
+    }
+    else if (state == 'NJ') {
         return income * 0.05 - deduction;
     }
 }
-
-var tax: number = calcTax(10000, 'NY', 3);
+var tax = calcTax(10000, 'NY', 3);
 console.log("Your tax is " + tax);
-
 var tax2 = calcTax(10000);
 console.log("Your another tax is " + tax2);
-
-var tax3_lambda = () => {
+var tax3_lambda = function () {
     var number = 150;
     return number * 4;
 };
 console.log("Tax 3 = " + tax3_lambda());
+//# sourceMappingURL=taxCalc.js.map
